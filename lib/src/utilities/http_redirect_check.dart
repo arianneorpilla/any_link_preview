ceeff42e90bd23ff1fbf13eb94fda170b81e997a
Arianne Orpilla <arianne@communic8.com>
2026-01-15 14:20:56 +1000

    Fix for file downloads

--- lib/src/utilities/http_redirect_check.dart
+++ lib/src/utilities/http_redirect_check.dart
@@ -11,8 +11,35 @@ Future<http.Response> fetchWithRedirects(
     ...headers,
     'User-Agent': userAgent ?? userAgentFallback,
   };
-  var response = await http.get(Uri.parse(url), headers: allHeaders);
+  
+  // First, check content type with HEAD request
+  var headResponse = await http.head(Uri.parse(url), headers: allHeaders);
   var redirectCount = 0;
+
+  // Follow redirects with HEAD
+  while (_isRedirect(headResponse) && redirectCount < maxRedirects) {
+    final location = headResponse.headers['location'];
+    if (location == null) {
+      throw Exception('HTTP redirect without Location header');
+    }
+
+    headResponse = await http.head(Uri.parse(location), headers: allHeaders);
+    redirectCount++;
+  }
+
+  if (redirectCount >= maxRedirects) {
+    throw Exception('Maximum redirect limit reached');
+  }
+
+  // Check if content type is appropriate (not a file download)
+  final contentType = headResponse.headers['content-type'];
+  if (contentType != null && _isFileContentType(contentType)) {
+    throw Exception('URL points to a file, not a web page');
+  }
+
+  // Now perform GET request
+  var response = await http.get(Uri.parse(url), headers: allHeaders);
+  redirectCount = 0;
 
   while (_isRedirect(response) && redirectCount < maxRedirects) {
     final location = response.headers['location'];
@@ -33,6 +60,35 @@ Future<http.Response> fetchWithRedirects(
 
 bool _isRedirect(http.Response response) {
   return [301, 302, 303, 307, 308].contains(response.statusCode);
+}
+
+bool _isFileContentType(String contentType) {
+  // Parse the content type to remove charset and other parameters
+  final mainType = contentType.split(';').first.trim().toLowerCase();
+  
+  // List of content types that are acceptable (web pages, APIs, etc.)
+  const acceptableTypes = [
+    'text/html',
+    'text/plain',
+    'application/json',
+    'application/ld+json',
+    'application/xml',
+    'text/xml',
+    'application/xhtml+xml',
+  ];
+  
+  // If it's an acceptable type, it's not a file
+  if (acceptableTypes.contains(mainType)) {
+    return false;
+  }
+  
+  // If it starts with text/, it's probably acceptable
+  if (mainType.startsWith('text/')) {
+    return false;
+  }
+  
+  // Everything else is considered a file (images, videos, PDFs, etc.)
+  return true;
 }
 
 Future<http.Response> getYoutubeData(
